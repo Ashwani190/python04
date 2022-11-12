28da85d58fc2ff8a07b7f2146e4bb40a0d86b643
Ashwani <112951391+Ashwani190@users.noreply.github.com>
2022-11-12 09:33:21 +0530

    Add files via upload

---
+++ fuctionIntro.py
@@ -0,0 +1,27 @@
+#def name(x):
+#    print(x)
+#name(4)    
+#name(10)
+
+
+#addition
+#def add(num1,num2):
+#    sum=num1+num2
+#    print(sum)
+#add(5,10)    
+#add(299,348)
+#add(45,65)
+
+#subtraction
+#def sub(num1,num2):
+#    sub=num1-num2
+#    return sub
+#result = sub(100,30)
+#print(result)
+
+#multiplication
+#def multi(num1,num2):
+#    multi=num1*num2
+#    return multi
+#result=multi(4,50)
+#print(result)
